588b0372944a5fa41aafd8d6b196ce2b02908473
Alfred Encinar <220027@fppiramide.com>
2022-11-15 09:24:30 +0100

    h

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,7 +1,8 @@
 //Crea la clase Pizza con los atributos y métodos necesarios.
 // Sobre cada pizza se necesita saber el tamaño.(mediana o familiar),
 // el tipo (margarita, cuatro quesos o marinera) y su estado (pedida o servida).
-// La clase debe almacenar información sobre el número total de pizzas que se han pedido y que se han servido.
+// La clase debe almacenar información sobre el número total de pizzas que se han pedido
+// y que se han servido.
 // Siempre que se crea una pizza nueva, su estado es "pedida".
 // Crea una clase de prueba que haga que responda como en el ejemplo de salida.
 
@@ -12,7 +13,11 @@ public class Pizza {
     private String tipo;
     private String estado;
 
-    public static void main(String[] args) {
+    Pizza(String ta, String ti, String es) {
+
+        this.tamaño = ta;
+        this.tipo = ti;
+        this.estado = es;
 
     }
 
