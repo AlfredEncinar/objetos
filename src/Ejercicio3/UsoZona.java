8bc1dce6dd11b3f83b58c4f7a339c7623e42625a
Alfred Encinar <220027@fppiramide.com>
2022-11-28 10:09:55 +0100

    h

---
+++ src/Ejercicio3/UsoZona.java
@@ -0,0 +1,34 @@
+package Ejercicio3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UsoZona {
+
+    public static void main(String[] args) {
+
+         Zona salaPrincipal = new Zona(1000);
+         Zona compraVenta = new Zona(200);
+         Zona zonaVip = new Zona(25);
+
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+
+        while (!salir) {
+            System.out.println("1. Mostrar número de entradas libres");
+            System.out.println("2. Vender entradas");
+            System.out.println("3. Salir");
+            opcion = sc.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Tenemos " + salaPrincipal.getEntradasPorVender() + " entradas de la sala principal");
+                System.out.println("Tenemos " + compraVenta.getEntradasPorVender() + " entradas de la zoona de compra venta");
+                System.out.println("Tenemos " + zonaVip.getEntradasPorVender() + " entradas de la zona vip");
+            } else if (opcion==2) {
+                System.out.println("efw ");
+            }else break;
+        }
+    }
+
+}
--- src/Ejercicio3/Zona.java
+++ src/Ejercicio3/Zona.java
@@ -1,5 +1,47 @@
+//Queremos gestionar la venta de entradas (no numeradas) de Expocoches Huesca que tiene 3 zonas:
+
+        //- La sala principal con 1000 entradas disponibles
+
+       //- La zona de compra-venta con 200 entradas disponbles
+
+        //- La zona vip con 25 entradas disponibles
+
+        //Hay que controlar que hay entradas disponibles antes de venderlas. La clase Zona con sus atributos y métodos se muestra a continuación
+
 package Ejercicio3;
 
 public class Zona {
+
+    private int entradasPorVender;
+
+    private int salaPrincipal;
+
+    private int compraVenta;
+
+    private int zonaVip;
+
+
+    public Zona(int n ){
+
+        this.entradasPorVender =n ;
+
+    }
+
+    public int getEntradasPorVender(){
+
+        return entradasPorVender;
+    }
+
+    public void vender(int n) {
+
+        if(this.entradasPorVender == 0) {
+            System.out.println("Lo siento, las entradas para esta zona están agotadas.");
+        } else if (this.entradasPorVender < n){
+            System.out.println("Sólo quedan " + this.entradasPorVender + " entradas para esa zona.");
+        } else {
+            entradasPorVender -= n;
+            System.out.println("Aquí tiene sus " + n + " entradas, gracias.");
+        }
+    }
 
 }
