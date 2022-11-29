3458ad89c2553d31573d22d259ca0830af5ac19d
Alfred Encinar <220027@fppiramide.com>
2022-11-29 10:21:01 +0100

    h

--- src/Ejercicio3/UsoZona.java
+++ src/Ejercicio3/UsoZona.java
@@ -26,7 +26,25 @@ public class UsoZona {
                 System.out.println("Tenemos " + compraVenta.getEntradasPorVender() + " entradas de la zoona de compra venta");
                 System.out.println("Tenemos " + zonaVip.getEntradasPorVender() + " entradas de la zona vip");
             } else if (opcion==2) {
-                System.out.println("efw ");
+                System.out.println("para que zona querias las entradas : ");
+                System.out.println("zona principal = pulse 1  / zona compra venta = pulse 2 / zona vip = pulse 3 ");
+                int aux = sc.nextInt();
+                if (aux == 1 ){
+                    System.out.println("hay " +  salaPrincipal.getEntradasPorVender() + " entradas ");
+                    System.out.println("¿Cuantas entradas queria?");
+                    sc.nextInt();
+                    System.out.println("¡Gracias!");
+                } else if (aux == 2) {
+                    System.out.println("hay " +  compraVenta.getEntradasPorVender() + "entradas");
+                    System.out.println("¿Cuantas entradas queria?");
+                    sc.nextInt();
+                    System.out.println("¡Gracias!");
+                } else if (aux == 3) {
+                    System.out.println("hay " +  zonaVip.getEntradasPorVender() + " entradas");
+                    System.out.println("¿Cuantas entradas queria?");
+                    sc.nextInt();
+                    System.out.println("¡Gracias!");
+                }
             }else break;
         }
     }
