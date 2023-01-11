bbb0c535a08ec7048e336a2be5c35b621e28035a
Alfred Encinar <encinar101@gmail.com>
2023-01-11 17:18:20 +0100

    h

--- src/Ejercicio7/PilaPalabras.java
+++ src/Ejercicio7/PilaPalabras.java
@@ -1,5 +1,18 @@
 package Ejercicio7;
 
+//Escribe una clase de nombre PilaPalabras,
+// para gestionar una estructura de pila que permita
+// apilar y desapilar objetos de la clase String.
+// La clase implementará el método apilarPalabra para poner
+// una palabra en la cima de la pila, el método desapilarPalabra
+// para quitar el elemento de la cima de la pila devolviéndolo
+// y el método obtenerPalabtaCima para obtener la palabra situada
+// en la cima de la pila sin quita4rla de ella. También implementrá
+// el método pilaPlabrasVacia para determinar si la pila está o no vacía
+// . Los metodos deven implementarse utilizando la clase LinkedList.
+//Escribe un programa que utilizando la clase PilaPalabras,
+// introduzca varias cadenas de caracteres en la pila y las desapile mostrándolas por pantalla.
+
 import java.util.LinkedList;
 
 public class PilaPalabras {
---
+++ src/Ejercicio8/UsoProducto.java
@@ -0,0 +1,12 @@
+package Ejercicio8;
+
+public class UsoProducto {
+    public static void main(String[] args) {
+
+
+        Producto p1 = new Producto(1, "Producto 1", "Tipo 1", 20, 10);
+        Producto p2 = new Producto(2, "Producto2", "tipo2", 40, 6);
+
+
+    }
+}
