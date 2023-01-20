21e59a3ae31682f2a3449fa8aa1c0c2240d109ef
Alfred Encinar <220027@fppiramide.com>
2023-01-20 13:48:23 +0100

    q

---
+++ src/Ejercicio9/ListaLIbros.java
@@ -0,0 +1,15 @@
+package Ejercicio9;
+
+//Desarrollar una lista de libros ordenada por título.
+// La funcionalidad de la lista será la habitual:
+// conocer el número de libros que hay en la lista,
+// insertar un nuevo libro (en la posición que corresponda),
+// eliminar el libro de una determinada posición
+// y obtener el libro de una determinada posición.
+// También incluirá un método para buscar un libro a partir de
+// una parte de su título (sin distinguir entre mayúsculas y minúsculas);
+// el método devolverá la posición en la que se
+// encuentra el libro (-1 si no lo encuentra).
+
+public class ListaLIbros {
+}
